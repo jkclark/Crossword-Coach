08fb70a858aef8bcc111d98c1223b79e0b52a0f0
Josh Clark <jclarktennis@gmail.com>
2025-05-23 13:24:15 -0400

    feat: filter out clues that reference other clues

--- scraping/src/index.ts
+++ scraping/src/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 
+import { CrosswordPuzzle } from "common/src/interfaces/CrosswordPuzzle";
 import FileDataStore from "storage/src/data-stores/FileDataStore";
 import { DataStore } from "storage/src/dataStore";
 import NYTSource from "./crossword-puzzle-sources/NYTSource";
@@ -16,22 +17,63 @@ async function main(cwpSource: CrosswordPuzzleSource, dataStore: DataStore) {
   /* Fetch each puzzle and store it */
   Promise.all(
     puzzleURLs.map(async (url) => {
-      /* Get the puzzle from the URL */
+      // Get the puzzle from the URL
       console.log(`Scraping puzzle from URL: ${url}`);
       const puzzle = await cwpSource.getPuzzle(url);
 
-      /* Filter theme clues */
+      // Filter theme clues
       const puzzleNoThemeClues = cwpSource.filterThemeClues(puzzle);
 
-      /* Store the puzzle */
-      await dataStore.savePuzzle(puzzleNoThemeClues);
+      // Filter clues that reference other clues
+      const puzzleNoCluesReferencingOtherClues = filterCluesThatReferenceOtherClues(puzzleNoThemeClues);
+
+      // Store the puzzle
+      await dataStore.savePuzzle(puzzleNoCluesReferencingOtherClues);
     })
   );
 }
 
+/**
+ * Filter clues that reference other clues from the crossword puzzle.
+ *
+ * @param puzzle The crossword puzzle to filter
+ * @returns A new crossword puzzle with clues that reference other clues filtered out
+ */
+function filterCluesThatReferenceOtherClues(puzzle: CrosswordPuzzle): CrosswordPuzzle {
+  /* Copy the original puzzle */
+  const filteredPuzzle = { ...puzzle };
+
+  /* Filter out clues that reference other clues */
+  filteredPuzzle.entries = puzzle.entries.filter((entry) => {
+    return !clueReferencesOtherClue(entry.clue);
+  });
+
+  console.log(
+    `Filtered out ${
+      puzzle.entries.length - filteredPuzzle.entries.length
+    } clues that reference other clues from ${puzzle.id}`
+  );
+  return filteredPuzzle;
+}
+
+/**
+ * Check if a clue references another clue.
+ *
+ * If the clue contains a string like "<number>-Across" or "<number>-Down",
+ * it is considered to reference another clue.
+ *
+ * @param clue The clue to check
+ * @returns True if the clue references another clue, false otherwise
+ */
+function clueReferencesOtherClue(clue: string): boolean {
+  const acrossRegex = /\d+-Across/;
+  const downRegex = /\d+-Down/;
+  return acrossRegex.test(clue) || downRegex.test(clue);
+}
+
 if (require.main === module) {
   /* Puzzle source */
-  const startDate = new Date("2023-01-01T00:00:00Z");
+  const startDate = new Date("2023-01-02T00:00:00Z");
   const endDate = new Date("2023-01-02T00:00:00Z");
   const nytCookie = process.env.NYT_COOKIE;
   if (!nytCookie) {
@@ -40,7 +82,7 @@ if (require.main === module) {
   const dummySource = new NYTSource(startDate, endDate, nytCookie);
 
   /* Data store */
-  const dummyDataStore = new FileDataStore("./temp/puzzles");
+  const dummyDataStore = new FileDataStore("../temp/puzzles_filtered");
 
   main(dummySource, dummyDataStore);
 }
