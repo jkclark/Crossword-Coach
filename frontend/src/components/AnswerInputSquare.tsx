6e7f8845d2142d1c7c2917e7107857a4f38c7477
Josh Clark <jclarktennis@gmail.com>
2025-06-27 17:02:29 -0400

    chore: use more appropriate theme colors

--- frontend/src/components/AnswerInputSquare.tsx
+++ frontend/src/components/AnswerInputSquare.tsx
@@ -12,14 +12,14 @@ const AnswerInputSquare: React.FC<AnswerInputSquareProps> = ({
       className={`
         flex items-center justify-center
         text-4xl font-bold
-        border border-black
+        border-secondary border-2
         aspect-square
         w-[clamp(2.5rem,8vw,4rem)] h-[clamp(2.5rem,8vw,4rem)]
         text-[clamp(1.5rem,5vw,3rem)]
         select-none
-        ${selected && !revealed ? "bg-primary" : ""}
+        ${selected && !revealed ? "bg-primary text-primary-content" : ""}
         ${jumping ? "animate-jump" : ""}
-        ${revealed ? "text-accent" : ""}
+        ${revealed ? "bg-secondary text-secondary-content" : ""}
       `}
     >
       {revealed ? answer : value}
