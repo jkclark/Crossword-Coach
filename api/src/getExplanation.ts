6cbd816a3bdbad708a4f14d66b294435b0308d6f
Josh Clark <jclarktennis@gmail.com>
2025-07-17 14:38:07 -0400

    feat: handle OPTIONS requests to explanation endpoint

--- api/src/getExplanation.ts
+++ api/src/getExplanation.ts
@@ -1,8 +1,16 @@
-import { APIGatewayProxyEvent, Handler } from "aws-lambda";
+import { APIGatewayProxyEvent, Context, Handler } from "aws-lambda";
 
 import { getCORSHeaders } from "./utils";
 
 export const handler: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204, // No Content for preflight requests
+      headers: getCORSHeaders(),
+      body: "",
+    };
+  }
+
   return {
     statusCode: 501, // Not Implemented
     headers: getCORSHeaders(),
