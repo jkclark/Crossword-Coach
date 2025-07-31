b54368ea6685f371485651572bd84be99026c0fc
Josh Clark <jclarktennis@gmail.com>
2025-07-31 16:07:34 -0400

    chore: set initial min/max to 4/10

--- frontend/src/state.ts
+++ frontend/src/state.ts
@@ -5,11 +5,6 @@ import type { GetEntriesFilterOptions } from "@crosswordcoach/storage";
 
 export const PAGE_SIZE = import.meta.env.VITE_ENTRIES_PAGE_SIZE;
 
-interface EntryFilterOptionsUI extends GetEntriesFilterOptions {
-  revealedLetters?: number;
-}
-export const entryFilterOptionsAtom = atom({} as EntryFilterOptionsUI);
-
 export const isLoadingEntriesAtom = atom(false);
 export const currentEntryPageAtom = atom<number | null>(null);
 export const currentEntryIndexAtom = atom<number | null>(null);
@@ -17,9 +12,21 @@ export const allEntriesAtom = atom({} as { [page: number]: Entry[] });
 
 export const MINIMUM_ANSWER_LENGTH = 3;
 export const MAXIMUM_ANSWER_LENGTH = 15;
-export const answerLengthMinAtom = atom<number>(MINIMUM_ANSWER_LENGTH);
-export const answerLengthMaxAtom = atom<number>(MAXIMUM_ANSWER_LENGTH);
+const INITIAL_ANSWER_LENGTH_MIN = 4;
+const INITIAL_ANSWER_LENGTH_MAX = 10;
+export const answerLengthMinAtom = atom<number>(INITIAL_ANSWER_LENGTH_MIN);
+export const answerLengthMaxAtom = atom<number>(INITIAL_ANSWER_LENGTH_MAX);
 export const revealedLettersAtom = atom<number>(2);
+
+interface EntryFilterOptionsUI extends GetEntriesFilterOptions {
+  revealedLetters?: number;
+}
+export const entryFilterOptionsAtom = atom({
+  answerLength: {
+    min: INITIAL_ANSWER_LENGTH_MIN,
+    max: INITIAL_ANSWER_LENGTH_MAX,
+  },
+} as EntryFilterOptionsUI);
 
 export const correctScoreAtom = atom<number>(0);
 export const totalScoreAtom = atom<number>(0);
