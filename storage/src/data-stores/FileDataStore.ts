c44d9352b368de0993ce4acafe693064326468bc
Josh Clark <jclarktennis@gmail.com>
2025-08-02 15:26:30 -0400

    chore: overwrite existing files, don't raise error

--- storage/src/data-stores/FileDataStore.ts
+++ storage/src/data-stores/FileDataStore.ts
@@ -24,19 +24,11 @@ export default class FileDataStore implements DataStore {
     throw new Error("getEntries not implemented on FileDataStore");
   }
 
-  // TODO: Save all clues to a file and see how big the file is
-  // We need to determine what one "entry" looks like for the frontend
-  // How would the frontend get the clues?
   async savePuzzle(puzzle: CrosswordPuzzle): Promise<void> {
     const puzzleFileName = `${puzzle.id}.json`;
     console.log(`Saving puzzle with ID = ${puzzle.id} to ${this.entryFolder}/${puzzleFileName}`);
 
-    /* If the file already exists, raise an error */
-    if (fs.existsSync(`${this.entryFolder}/${puzzleFileName}`)) {
-      throw new Error(`File ${this.entryFolder}/${puzzleFileName} already exists`);
-    }
-
-    /* Write the puzzle to a file */
+    /* Write the puzzle to a file, overwriting if it exists */
     const puzzleString = JSON.stringify(puzzle, null, 2);
     fs.writeFileSync(`${this.entryFolder}/${puzzleFileName}`, puzzleString, { encoding: "utf-8" });
   }
